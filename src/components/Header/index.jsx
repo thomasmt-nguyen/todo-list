ad7f913f6a53ef56bf2ca85f3d02f443210a7dc5
Thomas Nguyen <thomasmt.nguyen@gmail.com>
2023-07-27 15:49:51 -0700

    added elements to repo

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const Header = ({header}) => {
+    return <header id='todo-list-header'>{header}</header>
+}
+
+export default Header;
---
+++ src/components/TodoList/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../Header';
+
+const TodoList = () => {
+    return (
+        <div id='todo-list'>
+            <Header header='To Do List'/>
+        </div>
+    )
+}
+
+export default TodoList;
